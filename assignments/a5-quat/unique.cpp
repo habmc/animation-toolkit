1bbb105baee760f8dd03b85e74213a8571dec798
habmc <ptphan@brynmawr.edu>
2021-10-21 23:15:56 -0400

    uploaded code for assignment 5

--- assignments/a5-quat/unique.cpp
+++ assignments/a5-quat/unique.cpp
@@ -1,7 +1,89 @@
+#include <iostream>
 #include "atkui/framework.h"
+#include "atkmath/matrix3.h"
+#include <Eigen/Dense>
 using namespace glm;
 
-int main(int argc, char** argv) {
-   return 0;
-}
+class Unique : public atkui::Framework
+{
+public:
+   Unique() : atkui::Framework(atkui::Orthographic)
+   {
+   }
+   virtual void setup()
+   {
+      _mouseX = width() * 0.5;
+      _mouseY = height() * 0.5;
+      movingTarget = false;
 
+      for (int i = 5; i < width(); i += 10)
+      {
+         for (int j = 0; j < height(); j += 15)
+         {
+            cuboidPositions.push_back(vec3(i, j, 0));
+         }
+      }
+   }
+
+   virtual void scene()
+   {
+      for (int i = 0; i < cuboidPositions.size(); i += 1)
+      {
+         setColor(vec3(0.5, 0.8, 0.5));
+         vec3 cuboid = cuboidPositions[i];
+         if (movingTarget && pow(cuboidPositions[i][0] - _mouseX, 2) + pow(cuboidPositions[i][1] - _mouseY, 2) <= pow(100, 2))
+         {
+            float theta = atan2(_mouseY - cuboid[1], _mouseX - cuboid[0]);
+            push();
+            translate(cuboid);
+            rotate(theta, vec3(0, 0, 1));
+            drawCube(vec3(0), vec3(10, 1, 0));
+            pop();
+         }
+         else
+         {
+            drawCube(cuboid, vec3(10, 1, 0));
+         }
+      }
+
+      vec3 target = vec3(_mouseX, _mouseY, 0);
+      setColor(vec3(1, 0, 0));
+      drawSphere(target, targetRadius);
+   }
+
+   void mouseMotion(int pX, int pY, int dx, int dy)
+   {
+      if (movingTarget)
+      {
+         _mouseX = pX;
+         _mouseY = height() - pY;
+      }
+   }
+
+   void mouseDown(int button, int mods)
+   {
+      vec2 currentPosition = this->mousePosition();
+      if (button == GLFW_MOUSE_BUTTON_LEFT && length(currentPosition - vec2(_mouseX, height() - _mouseY)) <= targetRadius)
+      {
+         movingTarget = true;
+      }
+   }
+
+   void mouseUp(int button, int mods)
+   {
+      movingTarget = false;
+   }
+
+private:
+   int _mouseX;
+   int _mouseY;
+   bool movingTarget; // whether the target is being moved
+   std::vector<glm::vec3> cuboidPositions;
+   const double targetRadius = 10;
+};
+
+int main(int argc, char **argv)
+{
+   Unique viewer;
+   viewer.run();
+}
--- libsrc/atkmath/quaternion-basecode.cpp
+++ libsrc/atkmath/quaternion-basecode.cpp
@@ -2,33 +2,93 @@
 #include "atkmath/matrix3.h"
 #include "atkmath/vector3.h"
 
-namespace atkmath {
-
-Quaternion Quaternion::Slerp(const Quaternion& q0, const Quaternion& q1, double t)
+namespace atkmath
 {
-	// TODO
-	return Quaternion(1,0,0,0);
-}
 
-void Quaternion::toAxisAngle (Vector3& axis, double& angleRad) const
-{
-	// TODO
-}
+	Quaternion Quaternion::Slerp(const Quaternion &q0, const Quaternion &q1, double t)
+	{
+		// TODO
+		return Quaternion(1, 0, 0, 0);
+	}
 
-void Quaternion::fromAxisAngle (const Vector3& axis, double angleRad)
-{
-	// TODO
-}
+	void Quaternion::toAxisAngle(Vector3 &axis, double &angleRad) const
+	{
+		angleRad = acos(mW) * 2;
+		axis = Vector3(mX / sin(angleRad * 0.5), mY / sin(angleRad * 0.5), mZ / sin(angleRad * 0.5));
+	}
 
-Matrix3 Quaternion::toMatrix () const
-{
-	// TODO
-	return Matrix3::Identity;
-}
+	void Quaternion::fromAxisAngle(const Vector3 &axis, double angleRad)
+	{
+		mX = sin(angleRad * 0.5) * axis.x();
+		mY = sin(angleRad * 0.5) * axis.y();
+		mZ = sin(angleRad * 0.5) * axis.z();
+		mW = cos(angleRad * 0.5);
+		normalize();
+	}
 
-void Quaternion::fromMatrix(const Matrix3& rot)
-{
-	// TODO
-}
+	Matrix3 Quaternion::toMatrix() const
+	{
+		Matrix3 M;
+		double xx = mX * mX;
+		double xy = mX * mY;
+		double xz = mX * mZ;
+		double xw = mX * mW;
+		double yy = mY * mY;
+		double yz = mY * mZ;
+		double yw = mY * mW;
+
+		double zz = mZ * mZ;
+		double zw = mZ * mW;
+
+		M[0][0] = 1 - 2 * (yy + zz);
+		M[0][1] = 2 * (xy - zw);
+		M[0][2] = 2 * (xz + yw);
+
+		M[1][0] = 2 * (xy + zw);
+		M[1][1] = 1 - 2 * (xx + zz);
+		M[1][2] = 2 * (yz - xw);
+
+		M[2][0] = 2 * (xz - yw);
+		M[2][1] = 2 * (yz + xw);
+		M[2][2] = 1 - 2 * (xx + yy);
+		return M;
+	}
+
+	void Quaternion::fromMatrix(const Matrix3 &rot)
+	{
+		double wSquare = (1 + rot[0][0] + rot[1][1] + rot[2][2]) / 4;
+		double vxSquare = (1 + rot[0][0] - rot[1][1] - rot[2][2]) / 4;
+		double vySquare = (1 + rot[1][1] - rot[0][0] - rot[2][2]) / 4;
+		double vzSquare = (1 + rot[2][2] - rot[0][0] - rot[1][1]) / 4;
+		double max = std::max(std::max(std::max(wSquare, vxSquare), vySquare), vzSquare);
+		if (wSquare == max)
+		{
+			mW = sqrt(wSquare);
+			mZ = (rot[1][0] - rot[0][1]) / (4 * mW);
+			mX = (rot[2][1] - rot[1][2]) / (4 * mW);
+			mY = (rot[0][2] - rot[2][0]) / (4 * mW);
+		}
+		else if (vxSquare == max)
+		{
+			mX = sqrt(vxSquare);
+			mW = (rot[2][1] - rot[1][2]) / (4 * mX);
+			mY = (rot[0][2] - rot[2][0]) / (4 * mW);
+			mZ = (rot[1][0] - rot[0][1]) / (4 * mW);
+		}
+		else if (vySquare == max)
+		{
+			mY = sqrt(vySquare);
+			mW = (rot[0][2] - rot[2][0]) / (4 * mY);
+			mX = (rot[2][1] - rot[1][2]) / (4 * mW);
+			mZ = (rot[1][0] - rot[0][1]) / (4 * mW);
+		}
+		else
+		{
+			mZ = sqrt(vzSquare);
+			mW = (rot[1][0] - rot[0][1]) / (4 * mZ);
+			mX = (rot[2][1] - rot[1][2]) / (4 * mW);
+			mY = (rot[0][2] - rot[2][0]) / (4 * mW);
+		}
+	}
 
 }
