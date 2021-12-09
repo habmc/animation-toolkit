48cd46be9f1f6ee08558cc873835ae993c9013d2
habmc <ptphan@brynmawr.edu>
2021-12-08 23:11:16 -0500

    initial code

--- assignments/a11-steering/behaviors-basecode.cpp
+++ assignments/a11-steering/behaviors-basecode.cpp
@@ -7,18 +7,18 @@ using namespace atk;
 
 //--------------------------------------------------------------
 // Shared properties for all behaviors
-ABehavior::ABehavior(const char* name) : _name(name)
+ABehavior::ABehavior(const char *name) : _name(name)
 {
    // TODO: set good values
-   setParam("MaxSpeed", 1);
+   setParam("MaxSpeed", 35);
    setParam("AgentRadius", 1);
 }
 
 //--------------------------------------------------------------
 // Seek behavior
 
-ASeek::ASeek() : ABehavior("Seek") 
-{ 
+ASeek::ASeek() : ABehavior("Seek")
+{
 }
 
 //
@@ -29,16 +29,16 @@ ASeek::ASeek() : ABehavior("Seek")
 //
 // @note: call actor.getPosition to get teh actor's position
 // @note: call getParam("MaxSpeed") to get the max speed
-vec3 ASeek::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 ASeek::calculateDesiredVelocity(const ASteerable &actor,
+                                     const AWorld &world, const vec3 &target)
 {
-   return vec3(0,0,0);
+   return normalize(target - actor.getPosition()) * getParam("MaxSpeed");
 }
 
 //--------------------------------------------------------------
 // Flee behavior
 
-AFlee::AFlee() : ABehavior("Flee") 
+AFlee::AFlee() : ABehavior("Flee")
 {
 }
 
@@ -50,16 +50,16 @@ AFlee::AFlee() : ABehavior("Flee")
 //
 // @note: call actor.getPosition to get teh actor's position
 // @note: call getParam("MaxSpeed") to get the max speed
-vec3 AFlee::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& targetPos)
+vec3 AFlee::calculateDesiredVelocity(const ASteerable &actor,
+                                     const AWorld &world, const vec3 &targetPos)
 {
-    return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
 // Arrival behavior
 
-AArrival::AArrival() : ABehavior("Arrival") 
+AArrival::AArrival() : ABehavior("Arrival")
 {
    // TODO: Set good parameters
    setParam("kArrival", 1);
@@ -72,16 +72,16 @@ AArrival::AArrival() : ABehavior("Arrival")
 //
 // @param actor: steerable character to move
 // @return desired velocity
-vec3 AArrival::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& targetPos)
+vec3 AArrival::calculateDesiredVelocity(const ASteerable &actor,
+                                        const AWorld &world, const vec3 &targetPos)
 {
-    return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
 // Departure behavior
 
-ADeparture::ADeparture() : ABehavior("Departure") 
+ADeparture::ADeparture() : ABehavior("Departure")
 {
    setParam("InnerRadius", 1);
    setParam("OuterRadius", 1);
@@ -89,18 +89,18 @@ ADeparture::ADeparture() : ABehavior("Departure")
 }
 
 //
-// Calculate a repelent velocity based on the actor's 
+// Calculate a repelent velocity based on the actor's
 // distance from the target
-vec3 ADeparture::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& targetPos)
+vec3 ADeparture::calculateDesiredVelocity(const ASteerable &actor,
+                                          const AWorld &world, const vec3 &targetPos)
 {
-   return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
 // Avoid behavior
 
-AAvoid::AAvoid() : ABehavior("Avoid") 
+AAvoid::AAvoid() : ABehavior("Avoid")
 {
    setParam("kAvoid", 1);
 }
@@ -108,10 +108,10 @@ AAvoid::AAvoid() : ABehavior("Avoid")
 // If an actor is near an obstacle, avoid adds either a tangential or
 // normal response velocity
 //  Obstacles are in getWorld()->getObstacle(i) and have class type AObstacle
-vec3 AAvoid::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& targetPos)
+vec3 AAvoid::calculateDesiredVelocity(const ASteerable &actor,
+                                      const AWorld &world, const vec3 &targetPos)
 {
-    return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 //--------------------------------------------------------------
 // Wander behavior
@@ -122,10 +122,10 @@ AWander::AWander() : ABehavior("Wander")
 }
 
 // Wander returns a velocity whose direction changes randomly (and smoothly)
-vec3 AWander::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 AWander::calculateDesiredVelocity(const ASteerable &actor,
+                                       const AWorld &world, const vec3 &target)
 {
-   return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
@@ -137,17 +137,15 @@ ASeparation::ASeparation() : ABehavior("Separation")
    setParam("kSeparation", 1);
 }
 
-
 // Separation tries to maintain a constant distance between all agents
 // within the neighborhood of the agent
 // getWorld()->getAgent(i) returns the ith agent in the environment
 // you can check whether two agents are the same with ==
-vec3 ASeparation::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 ASeparation::calculateDesiredVelocity(const ASteerable &actor,
+                                           const AWorld &world, const vec3 &target)
 {
-   return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
-
 
 //--------------------------------------------------------------
 // Cohesion behavior
@@ -159,10 +157,10 @@ ACohesion::ACohesion() : ABehavior("Cohesion")
 }
 
 // Cohesion moves actors towards the center of a group of agents
-vec3 ACohesion::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 ACohesion::calculateDesiredVelocity(const ASteerable &actor,
+                                         const AWorld &world, const vec3 &target)
 {
-    return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
@@ -174,10 +172,10 @@ AAlignment::AAlignment() : ABehavior("Alignment")
 }
 
 // Alignment returns an average velocity of all active agents
-vec3 AAlignment::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 AAlignment::calculateDesiredVelocity(const ASteerable &actor,
+                                          const AWorld &world, const vec3 &target)
 {
-    return vec3(0);
+   return vec3(0);
 }
 
 //--------------------------------------------------------------
@@ -187,10 +185,10 @@ AFlocking::AFlocking() : ABehavior("Flocking")
 }
 
 // Flocking combines separation, cohesion, and alignment
-vec3 AFlocking::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 AFlocking::calculateDesiredVelocity(const ASteerable &actor,
+                                         const AWorld &world, const vec3 &target)
 {
-   return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
 
 //--------------------------------------------------------------
@@ -204,12 +202,10 @@ ALeader::ALeader() : ABehavior("Leader")
 }
 
 // You need to find the leader, who is always the first agent in agents
-// If the actor is the leader, move towards the target; otherwise, 
+// If the actor is the leader, move towards the target; otherwise,
 // follow the leader as a flock
-vec3 ALeader::calculateDesiredVelocity(const ASteerable& actor,
-   const AWorld& world, const vec3& target)
+vec3 ALeader::calculateDesiredVelocity(const ASteerable &actor,
+                                       const AWorld &world, const vec3 &target)
 {
-   return vec3(0,0,0);
+   return vec3(0, 0, 0);
 }
-
-
--- assignments/a11-steering/steerable-basecode.cpp
+++ assignments/a11-steering/steerable-basecode.cpp
@@ -4,37 +4,49 @@
 using namespace glm;
 using namespace atk;
 
-float ASteerable::kVelKv = 150.0; 
-float ASteerable::kOriKv = 150.0;  
-float ASteerable::kOriKp = 150.0;
+float ASteerable::kVelKv = 150.0;
+float ASteerable::kOriKv = 16.0;
+float ASteerable::kOriKp = 64.0;
 
-// Given a desired velocity, veld, and dt, compute a transform holding 
+// Given a desired velocity, veld, and dt, compute a transform holding
 // the new orientation and change in position
 //
 // rotation control: tau = I * (-Kv1 * thetaDot + Kp1 * (thetaD - theta))
 // translation control: f = m * Kv0 * (vd - v)
-void ASteerable::senseControlAct(const vec3& veld, float dt)
+void ASteerable::senseControlAct(const vec3 &veld, float dt)
 {
    // Compute _vd and _thetad
+   _vd = length(veld);
+   _thetad = atan2(veld[0], veld[2]);
 
    // compute _force and _torque
+   _force = _mass * kVelKv * (_vd - _state[VEL]);
+   _torque = _inertia * (-kOriKv * _state[AVEL] + kOriKp * (_thetad - _state[ORI]));
 
    // find derivative
+   _derivative[0] = _state[VEL];
+   _derivative[1] = _state[AVEL];
+   _derivative[2] = _force / _mass;
+   _derivative[3] = _torque / _inertia;
 
    // update state
+   _state[POS] += dt * _derivative[0];
+   _state[ORI] += dt * _derivative[1];
+   _state[VEL] += dt * _derivative[2];
+   _state[AVEL] += dt * _derivative[3];
 
    // compute global position and orientation and update _characterRoot
-   quat rot = glm::angleAxis(_state[ORI], vec3(0,1,0));
-   vec3 localPos(0,0,_state[POS]);
+   quat rot = glm::angleAxis(_state[ORI], vec3(0, 1, 0));
+   vec3 localPos(0, 0, _state[POS]);
 
    _characterRoot.setT(rot * localPos + _characterRoot.t());
-   _characterRoot.setR(rot); 
+   _characterRoot.setR(rot);
 }
 
 // randomize the colors, characters, and animation times here
 void ASteerable::randomizeAppearance()
 {
-   // to randomize the walking animations, compute different initial values 
+   // to randomize the walking animations, compute different initial values
    // for _time
 
    // to randomize color, call _drawer.setColor
@@ -42,4 +54,3 @@ void ASteerable::randomizeAppearance()
    // to randomize shape, compute random values for _drawer.setJointRadius
    // or randomly assign different drawers to have a mix of characters
 }
-
